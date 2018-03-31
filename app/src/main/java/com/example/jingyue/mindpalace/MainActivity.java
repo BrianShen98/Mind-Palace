016acb2834c6713a508897f48e12e46a610265bf
Jacob <Jacob@JacobdeMacBook-Pro.local>
2018-03-31 12:36:00 -0700

    first screen UI

--- app/src/main/java/com/example/jingyue/mindpalace/MainActivity.java
+++ app/src/main/java/com/example/jingyue/mindpalace/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.jingyue.mindpalace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_start = (Button) findViewById(R.id.start);
+        button_start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(getApplicationContext(), OptionScreen.class);
+               // startActivity(next);
+            }
+        });
     }
 }
