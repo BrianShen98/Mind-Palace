f1fa2400fbf4d9769c7ca91a176a74e6feb7c8f0
KeplerC <chenkaiyuan@ucla.edu>
2018-04-01 05:05:14 -0700

    ""

--- app/src/main/java/com/example/jingyue/mindpalace/MainActivity.java
+++ app/src/main/java/com/example/jingyue/mindpalace/MainActivity.java
@@ -124,7 +124,6 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        json_paser_for_label("");
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
