e2c3d79b3053e3aebe5c1f12274f9077a377a2c6
Jingyue Shen <Jingyue_shen@yahoo.com>
2018-04-01 06:31:21 -0700

    newest merge

--- app/src/main/java/com/example/jingyue/mindpalace/DisplayScreen.java
+++ app/src/main/java/com/example/jingyue/mindpalace/DisplayScreen.java
@@ -1,47 +1,291 @@
 package com.example.jingyue.mindpalace;
 
-/*import android.graphics.PointF;
-import android.os.Bundle;
 
-public class DisplayScreen extends MainActivity{
-    PointF pointStart1;
-    PointF pointStart2;
-
-    private LineView original;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        original = (LineView)findViewById(R.id.lineView);
-        pointStart1 = new PointF(10, 10);
-        pointStart2 = new PointF(10,10);
-        original.setPointA(pointStart1);
-        original.setPointB(pointStart2);
-
-        original.draw();
-    }
-
-}
-*/
-
-import android.app.Activity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class DisplayScreen extends Activity {
-    LineView drawView;
+public class DisplayScreen extends FragmentActivity{
+
+    /**
+     * Hold a reference to the current animator, so that it can be canceled mid-way.
+     */
+    private Animator mCurrentAnimator;
+
+    /**
+     * The system "short" animation time duration, in milliseconds. This duration is ideal for
+     * subtle animations or animations that occur very frequently.
+     */
+    private int mShortAnimationDuration;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_screen);
 
-        drawView = new LineView(this);
-        drawView.setBackgroundColor(Color.WHITE);
-        setContentView(drawView);
+        ImageView src_img = (ImageView)findViewById(R.id.src_img);
+        src_img.bringToFront();
+        //back-to-mainpage button
+        final Button button_back_to_front = (Button) findViewById(R.id.back_to_front);
+        button_back_to_front.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(next);
+            }
+        });
 
+        //display text button
+        final Button button_display_txt = (Button) findViewById(R.id.display_txt);
+        button_display_txt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                displayTXT();
+            }
+        });
+
+        //4 lines
+        LineView line1 = (LineView)findViewById(R.id.path1);
+        line1.bringToFront();
+        LineView line2 = (LineView)findViewById(R.id.path2);
+        LineView line3 = (LineView)findViewById(R.id.path3);
+        LineView line4 = (LineView)findViewById(R.id.path4);
+
+
+        line1.init(430,640,325,330);
+        line2.init(430,700,330,1200);
+        line3.init(725,690,805,1190);
+        line4.init(725,640,805,330);
+
+// Hook up clicks on the thumbnail views.
+
+        final View thumb1View = findViewById(R.id.thumb_button_1);
+        thumb1View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomImageFromThumb(thumb1View, R.drawable.background);
+            }
+        });
+
+        final View thumb2View = findViewById(R.id.thumb_button_2);
+        thumb2View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomImageFromThumb(thumb2View, R.drawable.background);
+            }
+        });
+
+        final View thumb3View = findViewById(R.id.thumb_button_3);
+        thumb3View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomImageFromThumb(thumb3View, R.drawable.background);
+            }
+        });
+
+        final View thumb4View = findViewById(R.id.thumb_button_4);
+        thumb4View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomImageFromThumb(thumb4View, R.drawable.background);
+            }
+        });
+
+
+        // Retrieve and cache the system's default "short" animation time.
+        mShortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Navigate "up" the demo structure to the launchpad activity.
+                // See http://developer.android.com/design/patterns/navigation.html for more.
+                NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void displayTXT()
+    {
+        TextView textView1 = (TextView) findViewById(R.id.text1);
+        TextView textView2 = (TextView) findViewById(R.id.text2);
+        TextView textView3 = (TextView) findViewById(R.id.text3);
+        TextView textView4 = (TextView) findViewById(R.id.text4);
+
+        ImageView imgView1 = (ImageView) findViewById(R.id.img1);
+        ImageView imgView2 = (ImageView) findViewById(R.id.img2);
+        ImageView imgView3 = (ImageView) findViewById(R.id.img3);
+        ImageView imgView4 = (ImageView) findViewById(R.id.img4);
+
+        textView1.setVisibility(View.VISIBLE);
+        textView2.setVisibility(View.VISIBLE);
+        textView3.setVisibility(View.VISIBLE);
+        textView4.setVisibility(View.VISIBLE);
+
+        imgView1.setVisibility(View.INVISIBLE);
+        imgView2.setVisibility(View.INVISIBLE);
+        imgView3.setVisibility(View.INVISIBLE);
+        imgView4.setVisibility(View.INVISIBLE);
+
+    }
+
+
+    /**
+     * "Zooms" in a thumbnail view by assigning the high resolution image to a hidden "zoomed-in"
+     * image view and animating its bounds to fit the entire activity content area. More
+     * specifically:
+     *
+     * <ol>
+     *   <li>Assign the high-res image to the hidden "zoomed-in" (expanded) image view.</li>
+     *   <li>Calculate the starting and ending bounds for the expanded view.</li>
+     *   <li>Animate each of four positioning/sizing properties (X, Y, SCALE_X, SCALE_Y)
+     *       simultaneously, from the starting bounds to the ending bounds.</li>
+     *   <li>Zoom back out by running the reverse animation on click.</li>
+     * </ol>
+     *
+     * @param thumbView  The thumbnail view to zoom in.
+     * @param imageResId The high-resolution version of the image represented by the thumbnail.
+     */
+    private void zoomImageFromThumb(final View thumbView, int imageResId) {
+        // If there's an animation in progress, cancel it immediately and proceed with this one.
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+
+        // Load the high-resolution "zoomed-in" image.
+        final ImageView expandedImageView = (ImageView) findViewById(R.id.img1);
+        expandedImageView.setImageResource(imageResId);
+
+        // Calculate the starting and ending bounds for the zoomed-in image. This step
+        // involves lots of math. Yay, math.
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        // The start bounds are the global visible rectangle of the thumbnail, and the
+        // final bounds are the global visible rectangle of the container view. Also
+        // set the container view's offset as the origin for the bounds, since that's
+        // the origin for the positioning animation properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.container).getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        // Adjust the start bounds to be the same aspect ratio as the final bounds using the
+        // "center crop" technique. This prevents undesirable stretching during the animation.
+        // Also calculate the start scaling factor (the end scaling factor is always 1.0).
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation begins,
+        // it will position the zoomed-in view in the place of the thumbnail.
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations to the top-left corner of
+        // the zoomed-in view (the default is the center of the view).
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // Construct and run the parallel animation of the four translation and scale properties
+        // (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBounds.left,
+                        finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBounds.top,
+                        finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X, startScale, 1f))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimator = null;
+            }
+        });
+        set.start();
+        mCurrentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down to the original bounds
+        // and show the thumbnail instead of the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimator != null) {
+                    mCurrentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel, back to their
+                // original values.
+                AnimatorSet set = new AnimatorSet();
+                set
+                        .play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView, View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView, View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimator = set;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jingyue/mindpalace/LineView.java
+++ app/src/main/java/com/example/jingyue/mindpalace/LineView.java
@@ -1,93 +1,82 @@
 package com.example.jingyue.mindpalace;
 
-/*import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PointF;
-import android.support.annotation.Nullable;
-import android.util.AttributeSet;
-import android.view.View;
 
-public class LineView extends View {
 
-    private Paint paint = new Paint();
-    private PointF pointA, pointB;
-    public LineView(Context context) {
-        super(context);
-    }
-
-    public LineView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public LineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(20);
-        canvas.drawLine(pointA.x,pointA.y,pointB.x,pointB.y,paint);
-        super.onDraw(canvas);
-    }
-
-    public void setPointA(PointF point)
-    {
-        pointA = point;
-    }
-
-    public void setPointB(PointF point)
-    {
-        pointB = point;
-    }
-
-    public void draw()
-    {
-        invalidate();
-        requestLayout();
-    }
-}
-*/
-
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.util.AttributeSet;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
-import android.graphics.Point;
-import android.view.Display;
-import android.support.annotation.Nullable;
+import android.graphics.Path;
+import android.graphics.PathEffect;
+import android.graphics.PathMeasure;
+import android.util.AttributeSet;
 import android.view.View;
+import android.util.Log;
 
-public class LineView extends View {
-    Paint paint = new Paint();
+public class LineView extends View
+{
+    Path path;
+    Paint paint;
+    float length;
 
-    private void init() {
-        paint.setColor(Color.BLACK);
-    }
-
-    public LineView(Context context) {
+    public LineView(Context context)
+    {
         super(context);
-        init();
     }
 
-    public LineView(Context context, @Nullable AttributeSet attrs) {
+    public LineView(Context context, AttributeSet attrs)
+    {
         super(context, attrs);
-        init();
     }
 
-    public LineView(Context context, @Nullable AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init();
+    public LineView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
     }
 
+    public void init(int start_x,int start_y,int end_x,int end_y)
+    {
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+
+        path = new Path();
+        path.moveTo(start_x, start_y);
+        path.lineTo(end_x, end_y);
+
+
+        // Measure the path
+        PathMeasure measure = new PathMeasure(path, false);
+        length = measure.getLength();
+
+        float[] intervals = new float[]{length, length};
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(LineView.this, "phase", 1.0f, 0.0f);
+        animator.setDuration(1000);
+        animator.start();
+    }
+
+    //is called by animtor object
+    public void setPhase(float phase)
+    {
+        Log.d("LineView","setPhase called with:" + String.valueOf(phase));
+        paint.setPathEffect(createPathEffect(length, phase, 0.0f));
+        invalidate();//will calll onDraw
+    }
+
+    private static PathEffect createPathEffect(float pathLength, float phase, float offset)
+    {
+        return new DashPathEffect(new float[] { pathLength, pathLength },
+                Math.max(phase * pathLength, offset));
+    }
 
     @Override
-    public void onDraw(Canvas canvas) {
-        canvas.drawLine(0, 0, 20, 20, paint);
-        canvas.drawLine(20, 0, 0, 20, paint);
+    public void onDraw(Canvas c)
+    {
+        super.onDraw(c);
+        c.drawPath(path, paint);
     }
-
 }
